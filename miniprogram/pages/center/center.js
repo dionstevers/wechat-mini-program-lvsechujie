f6fcbecd9d7a131bacc05ff9db10ab4504d34cc4
Ydz0616 <107053257+Ydz0616@users.noreply.github.com>
2022-11-02 00:55:46 +0800

    low-carbon journal enhanced

--- miniprogram/pages/center/center.js
+++ miniprogram/pages/center/center.js
@@ -9,52 +9,55 @@ function initChart(canvas,width,height,dpr){
     devicePixelRatio:dpr
   })
   canvas.setChart(chart)
-  const model = {
-    yCates: ['5', '4', '3',
-      '2', '1'],
-    xCates: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri','Sat','Sun'],
-    data: [
-      // [yCateIndex, xCateIndex, value]
-      // 替换为云数据库data
-      [0, 0, 1], [0, 1, 7], [0, 2, 3], [0, 3, 5], [0, 4, 2],[0,5,1],[0,6,1],
-      [1, 0, 1], [1, 1, 2], [1, 2, 4], [1, 3, 8], [1, 4, 2],[1,5,1],[1,6,3],
-      [2, 0, 2], [2, 1, 3], [2, 2, 8], [2, 3, 6], [2, 4, 7],[2,5,1],[2,6,1],
-      [3, 0, 3], [3, 1, 7], [3, 2, 5], [3, 3, 1], [3, 4, 6],[3,5,1],[3,6,4],
-      [4, 0, 3], [4, 1, 2], [4, 2, 7], [4, 3, 8], [4, 4, 9],[4,5,1],[4,6,1],
-      [5, 0, 2], [5, 1, 2], [5, 2, 3], [5, 3, 4], [5, 4, 7],[5,5,1],[5,6,1],
-      [6, 0, 6], [6, 1, 5], [6, 2, 3], [6, 3, 1], [6, 4, 2],[6,5,1],[6,6,2]
-    ]
-  };
 
-  const data = model.data.map(function (item) {
-    return [item[1], item[0], item[2] || '-'];
-  });
+  
+const heat = 3;
 
+function getVirtulData(year) {
+  year = year || '2022';
+  var date = +echarts.number.parseDate(year + '-05-01');
+  var end = +echarts.number.parseDate(year + '-05-31');
+  var dayTime = 3600 * 24 * 1000;
+  var data = [];
+  for (var time = date; time <= end; time += dayTime) {
+      data.push([
+          echarts.format.formatTime('yyyy-MM-dd', time),
+          heat
+      ]);
+  }
+  return data;
+}
+
+chart.resize()
   const option = {
     // tooltip: {
     //   position: 'top'
     // },
     animation: true,
     grid: {
-      bottom: 20,
+      bottom: 10,
       top: 10,
       left:40,
       right:20
     },
-    xAxis: {
-      type: 'category',
-      data: model.xCates
-    },
-    yAxis: {
-      type: 'category',
-      data: model.yCates
+    
+    calendar : {
+      range : '2022-05',
+      top:'25',
+      left:'center',
+      orient:'vertical',
+      width:'230rpx',
+      yearLabel: false,
+      monthLabel: false
+      
     },
     visualMap: {
       min: 1,
       max: 10,
       show: false,
       calculable: true,
-      orient: 'horizontal',
+      orient: 'vertical',
+      top: 'middle',
       left: 'center',
       bottom: 10,
       inRange: {
@@ -64,7 +67,8 @@ function initChart(canvas,width,height,dpr){
     series: [{
       name: 'Punch Card',
       type: 'heatmap',
-      data: data,
+      coordinateSystem: 'calendar',
+      // data: getVirtulData(2022),
       label: {
         normal: {
           show: false
--- miniprogram/pages/journal/journal.ts
+++ miniprogram/pages/journal/journal.ts
@@ -5,9 +5,49 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    forminfo :'',
+    userInfo: '',
+    
+    questions:[
+      {id:1,value:"未使用一次性餐具",isdisabled:''},
+      {id:2,value:"乘坐公共交通或骑自行车出行",isdisabled:''}
+      
+    ]
   },
-
+  formSubmit: function(e: { detail: { value: any; }; }){
+     
+    const data = wx.getStorageSync('userInfo')
+    
+    // 提交低碳日记-->credit+20
+    this.setData({
+      forminfo:e.detail.value
+    })
+    const db  = wx.cloud.database();
+    const _ = db.command;
+    db.collection('userInfo').doc(data._id).update({
+      data:{
+        credit:_.inc(20)
+      }
+    })
+    
+    // 将form数据写入数据库
+    db.collection('formdata').add({
+      data:{
+        time:  Date(),
+        form: e.detail.value,
+      }
+    })
+    
+ 
+  
+     
+  },
+  checkSubmit(){},
+  getUserInfo(){
+    this.setData({
+      userInfo: wx.getStorageSync('userInfo')
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -23,12 +63,12 @@ Page({
   onReady() {
 
   },
-
+   
   /**
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+      this.getUserInfo()
   },
 
   /**
