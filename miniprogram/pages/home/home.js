5813f9830cd0e06b16e24b019df28b2584439954
Tianshu Feng <fengtianshu38@126.com>
2022-11-15 14:47:00 +0800

    add AQI info

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -25,7 +25,11 @@ Page({
     ],
     index: 0,
 
-    isFront: true
+    isFront: true,
+
+    aqi: '',
+    name: '',
+    category: ''
   },
 
   onShow() {
@@ -43,7 +47,45 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad() {
+  calcDist: async function (triplet, i) {
+    var _this = this
+    var dist = -1
+    const db = wx.cloud.database()
+    return db.collection('monitor').where({
+        POI_ID: triplet[2][i].id,
+      })
+      .get().then(res => {
+        //console.log(res.data)
+        var mlat = res.data[0].POI_Latitude
+        var mlng = res.data[0].POI_Longitude
+        return _this.GetDistance(triplet[0], triplet[1], mlat, mlng)
+      })
+  },
+
+  async findClosest(triplet) {
+    var _this = this
+    var closest = -1
+    var closestDist = -1
+    for (var i in triplet[2]) {
+      var dist = await _this.calcDist(triplet, i)
+      console.log(i, dist)
+      if (closest == -1 || dist < closestDist) {
+        closest = i
+        closestDist = dist
+      }
+    }
+    return closest
+  },
+  updateAQI: function (res, closest) {
+    console.log(closest)
+    this.setData({
+      aqi: res.data.station[closest].aqi,
+      name: res.data.station[closest].name,
+      category: res.data.station[closest].category
+    })
+  },
+
+  onLoad: async function() {
     const db = wx.cloud.database()
     const _ = db.command
     let _this = this
@@ -68,6 +110,24 @@ Page({
             var now = new Date()
             now.setHours(0, 0, 0, 0)
             console.log(now)
+
+            wx.getLocation({
+              type: 'gcj02',
+              success(loc) {
+                var latitude = loc.latitude.toFixed(2)
+                var longitude = loc.longitude.toFixed(2)
+                console.log(longitude, latitude)
+                wx.request({
+                  url: "https://devapi.qweather.com/v7/air/now?key=df35576dc85c4dd19641b86b91b48190&location=" + longitude + ',' + latitude,
+                  success: async function (res) {
+                    _this.updateAQI(res, await _this.findClosest([latitude, longitude, res.data.station]))
+                  },
+                  fail: function (err) {
+                    console.log(err)
+                  }
+                })
+              }
+            })
 
             // 查询上次未结束的tracking？
             db.collection('track').orderBy('date', 'desc').limit(1).where({
