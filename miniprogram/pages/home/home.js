86833b6e08d17512846dd50b809367845637ebca
Yuandong <yz743@duke.edu>
2023-12-12 20:41:36 -0500

    Revert "Update home.js"
    This reverts commit f93781158a4cf1fc0b2eefb75ef2cb11a67499bd.

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -301,20 +301,15 @@ Page({
               },
               success: function (result) {
                 console.log(result)
-                db.collection('track').doc(_this.data.curID).get({
+                db.collection('track').doc(_this.data.curID).update({
                   success: function (res) {
                     console.log(res.data.points)
-                    var gap = 60;
-                    var j = Math.floor(cnt / 10);
-                    console.log(j)
-                    if (j >= gap * 3) {
-                      var d30 = _this.GetDistance(res.data.points[j - gap * 3].latitude, res.data.points[j - gap * 3].longitude, res.data.points[j].latitude, res.data.points[j].longitude)
-                      var d20 = _this.GetDistance(res.data.points[j - gap * 2].latitude, res.data.points[j - gap * 2].longitude, res.data.points[j].latitude, res.data.points[j].longitude)
-                      var d10 = _this.GetDistance(res.data.points[j - gap].latitude, res.data.points[j - gap].longitude, res.data.points[j].latitude, res.data.points[j].longitude)
-                      console.log(d10);
-                      console.log(d20);
-                      console.log(d30);
-                      if (d30 < 1e-6 && d20 < 1e-6 && d10 < 1e-6) {
+                    if (cnt >= 180) {
+                      d30 = _this.GetDistance(res.data.points[j - 180].latitude, res.data.points[j - 180].longitude, res.data.points[j].latitude, res.data.points[j].longitude)
+                      d20 = _this.GetDistance(res.data.points[j - 120].latitude, res.data.points[j - 120].longitude, res.data.points[j].latitude, res.data.points[j].longitude)
+                      d10 = _this.GetDistance(res.data.points[j - 120].latitude, res.data.points[j - 120].longitude, res.data.points[j].latitude, res.data.points[j].longitude)
+                      console.log(d10, d20, d30);
+                      if (d30 < 1e-9 && d20 < 1e-9 && d10 < 1e-9) {
                         _this.endTrack(false);
                       }
                     }
@@ -487,20 +482,15 @@ Page({
                     },
                     success: function (result) {
                       console.log(result)
-                      db.collection('track').doc(_this.data.curID).get({
+                      db.collection('track').doc(_this.data.curID).update({
                         success: function (res) {
                           console.log(res.data.points)
-                          var gap = 60;
-                          var j = Math.floor(cnt / 10);
-                          console.log(j)
-                          if (j >= gap * 3) {
-                            var d30 = _this.GetDistance(res.data.points[j - gap * 3].latitude, res.data.points[j - gap * 3].longitude, res.data.points[j].latitude, res.data.points[j].longitude)
-                            var d20 = _this.GetDistance(res.data.points[j - gap * 2].latitude, res.data.points[j - gap * 2].longitude, res.data.points[j].latitude, res.data.points[j].longitude)
-                            var d10 = _this.GetDistance(res.data.points[j - gap].latitude, res.data.points[j - gap].longitude, res.data.points[j].latitude, res.data.points[j].longitude)
-                            console.log(d10);
-                            console.log(d20);
-                            console.log(d30);
-                            if (d30 < 1e-6 && d20 < 1e-6 && d10 < 1e-6) {
+                          if (cnt >= 180) {
+                            d30 = _this.GetDistance(res.data.points[j - 180].latitude, res.data.points[j - 180].longitude, res.data.points[j].latitude, res.data.points[j].longitude)
+                            d20 = _this.GetDistance(res.data.points[j - 120].latitude, res.data.points[j - 120].longitude, res.data.points[j].latitude, res.data.points[j].longitude)
+                            d10 = _this.GetDistance(res.data.points[j - 60].latitude, res.data.points[j - 60].longitude, res.data.points[j].latitude, res.data.points[j].longitude)
+                            console.log(d10, d20, d30);
+                            if (d30 < 1e-9 && d20 < 1e-9 && d10 < 1e-9) {
                               _this.endTrack(false);
                             }
                           }
