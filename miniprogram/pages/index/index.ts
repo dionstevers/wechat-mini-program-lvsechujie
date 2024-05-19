879c1d97e72547d980bf842e29ffac0bbdec665a
Yuandong <yz743@duke.edu>
2024-05-19 16:49:53 +0800

    center: shareFromid recorded
    - sharer id (if any) recorded
    - stored to db
    - removed auto-login

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -17,27 +17,47 @@ Page({
    */
   
   async onLoad(options) {
+    // share app message
       console.log('the openid of the one who shared is ', options.id)
+      const sharedFromid = options.id
       const db = wx.cloud.database()
+
+
       try{
         const res = await wx.cloud.callFunction({ name: 'login' });
         this.setData({ openID: (res.result as {data?:any}).data._openid });
         app.globalData.openID = (res.result as {data?:any}).data._openid;
-        const userInfoQuery = await db.collection('userInfo')
-          .where({ _openid: this.data.openID })
-          .get();
-        if (userInfoQuery.data.length === 1) {
-          app.globalData.userInfo = userInfoQuery.data[0];
-          wx.showToast({ 
-          title:'正在登录中',
-          icon:'loading',
-          duration:1500 });
-          setTimeout(() => {
-            wx.switchTab({
-              url : '/pages/center/center'
-            })
-        }, 1500);
-        }}catch(err){
+        const openid = (res.result as {data?:any}).data._openid;
+
+        // now include sharedFromid upload
+        if(sharedFromid==null){
+          console.log('no share user')
+          return;
+        }
+        // check if the sharedFromid is null
+        if(sharedFromid!=null){
+          // get the entry
+          const userDoc = await db.collection('shareNet').where({
+            _id: openid
+          }).get();
+
+          // check if the entry exists
+          if(userDoc.data.length>0){
+            await db.collection('shareNet').doc(openid).update({
+              data: {
+                sharedFromid: db.command.addToSet(sharedFromid) // Add to the array if not already present
+              }
+            });
+          }else{//if the entry does not exist, then create it
+            await db.collection('shareNet').add({
+              data: {
+                _id: openid, // Use openid as the document id
+                sharedFromid: [sharedFromid],
+              }
+            });
+          }
+        }
+      }catch(err){
         console.log(err)
       }
     
@@ -46,9 +66,6 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady(){
-
-  },
 
   async HandleSignUp() {
     const db = wx.cloud.database()
@@ -64,7 +81,7 @@ Page({
         wx.showModal({ title: '您已有账号',content:'请直接点击登录按钮登录' , showCancel:false});
       }else{
         wx.navigateTo({
-          url:'/pages/login/login'
+          url:'/pages/information/information'
         })
       }
 
@@ -108,7 +125,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-    
+
   },
 
   /**
@@ -117,15 +134,6 @@ Page({
   onHide() {
 
   },
-  
-  onShareTimeline(){
-    return {
-      title:"快来一起低碳出街",
-      query:this.data.openID,
-      imageUrl: "https://696c-iluvcarb-0gzvs45g82b57f98-1315168954.tcb.qcloud.la/logo/WechatIMG778.jpg?sign=c7c5732217972f1c9393850e9e040d70&t=1713096313"
-    }
-  },
-
 
   /**
    * 生命周期函数--监听页面卸载
@@ -152,6 +160,16 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage() {
-
+    return {
+      title: "快来一起低碳出街~",
+      path:"/pages/index/index?id=" + this.data.openID,
+      imageUrl: "https://696c-iluvcarb-0gzvs45g82b57f98-1315168954.tcb.qcloud.la/logo/WechatIMG778.jpg?sign=c7c5732217972f1c9393850e9e040d70&t=1713096313",
+      success: function(res){
+        console.log(res.shareTickets[0])
+      },
+      fail:function(res){
+        console.log('share failed')
+      }
+    }
   }
 })
