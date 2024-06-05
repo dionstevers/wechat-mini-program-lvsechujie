c0206c62131ccbbe59015baf5a0fb00e406606a9
Yuandong <yz743@duke.edu>
2024-06-05 21:55:46 +0800

    test: send cash

--- miniprogram/pages/aboutus/aboutus.ts
+++ miniprogram/pages/aboutus/aboutus.ts
@@ -1,3 +1,5 @@
+
+import { hexMD5 } from '../../utils/md5.js';
 // pages/survey/survey.ts
 export{}
 const app = getApp()
@@ -7,33 +9,91 @@ Page({
    */
   data: {
     userInfo: null,
-    background : 'linear-gradient(180deg, #00022a 0%,#009797 100%);'
+    background : 'linear-gradient(180deg, #00022a 0%,#009797 100%);',
+    u_openid:''
+
   },
   /**
    * 生命周期函数--监听页面加载
    */
 
-  onLoad() {
+  onLoad(option) {
+    console.log(option)
+    console.log('the u_openid is ',option.u_openid)
+    this.setData({
+      u_openid : option.u_openid
+    });
 
   },
+  dailyOrderid(){
+    const date = new Date();
+    const orderid = date.getDate()+'-'+date.getDay()+'-'+date.getFullYear()+'-'+getApp().globalData.openID;
+    return orderid;
+  },
 
+  HandleSendCash(){
+    if(this.data.u_openid == ''){
+      return;
+    }
+    const u_openid = this.data.u_openid;
+    const uid = '10815051';
+    const orderid = this.dailyOrderid();
+    const type = '0';
+    const money = '50';
+    let reqtick = Date.parse(new Date().toString());
+    reqtick = reqtick/1000;
+    const openid = u_openid;
+    const apikey = 'carbclever';
+    const sig = hexMD5(uid + type + orderid + money + reqtick + openid + apikey);
+    wx.request({
+      url: 'https://mp001.yaoyaola.net/exapi/SendRedPackToOpenid?uid='+uid+'&type='+type+'&orderid='+orderid+'&money='+money+'&reqtick='+reqtick+'&openid='+openid+'&sign='+sig+'&title=现金发奖' + '&sendname=碳行家&wishing=心想事成',
+      success(res){
+        console.log('response from server',res)
+        wx.showModal({
+          title:'恭喜！',
+          content:'您的现金红包已发放',
+          showCancel:false
+        })
+      },
+      fail(res){
+        console.log('response not found',res)
+        wx.showToast({
+          title:'请稍后再试',
+          icon:'error',
+          duration:1000
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
 
   },
-
+  // testApi(){
+  //   wx.request({
+  //     url:"https://mp001.yaoyaola.net/exapi/check_user/10815051?mp=1&url=/pages/aboutus/aboutus&flag=0",
+  //     success(res){
+  //       console.log('the res message is', res)
+  //     },
+  //     fail(err){
+  //       console.log(err)
+  //     }
+  //   })
+  // },
   /**
    * 生命周期函数--监听页面显示
    */
   onShow() {
+    this.dailyOrderid();
     this.setData({
       userInfo : app.globalData.userInfo
     })
     wx.setNavigationBarTitle({
       title: '碳行家｜关于我们'
     })
+    // this.testApi()
   },
 
   /**
