ed1ebeb44276356a6168b23ec1d9cc4c7433baa4
Yuandong <yz743@duke.edu>
2024-07-02 18:04:52 +0800

    center: weekly transporation mode analysis

--- miniprogram/pages/center/center.js
+++ miniprogram/pages/center/center.js
@@ -2,93 +2,210 @@
 import { logEvent } from '../../utils/log';
 import * as echarts from "../../asset/ec-canvas/echarts"
 import { onHandleSignIn } from '../../utils/login'
+import { getWeekRange } from "../../utils/time"
 import { updateColor } from '../../utils/colorschema'
 const app = getApp();
 
+// function initChart(canvas, width, height, dpr) {
+//   const chart = echarts.init(canvas, null, {
+//     width: width,
+//     height: height,
+//     devicePixelRatio: dpr // new
+//   });
+//   canvas.setChart(chart);
+//   const db = wx.cloud.database()
+//   const _ = db.command
+//   const $ = db.command.aggregate;
+//   var { firstDayOfWeek, lastDayOfWeek } = getWeekRange();
+//   var walking = 0
+//   var pub_transit = 0
+//   var driving = 0
+//   wx.cloud.callFunction({
+//     name: 'login',
+//     success: id_res => {
+//       db.collection('track').where({
+//         _openid: id_res.result.data._openid,
+//       })
+//       .orderBy('date', 'desc')
+//       .get({
+//         success: function (res) {
+//           let list = res.data
+//           console.log(id_res.result.data._openid)
+//           console.log('get list:', list);
+
+//           list.forEach(item => {
+//             if (item['transport'] == "步行/自行车" || item['transport'] == "电动自行车" || item['transport'] == "步行或骑行") cycling += 1
+//             else if (item['transport'] == "公交/出租车/网约车/轨道交通" || item['transport'] == "公共交通") pub_transit += 1
+//             else driving += 1
+//           })
+//           console.log(cycling)
+//           console.log(pub_transit)
+//           console.log(driving)
+//           var option = {
+//             backgroundColor: 'rgba(0, 0, 0, 0)',
+//             legend: {
+//               orient: 'vertical',
+//               x: 'left',
+//               data: ['步行/骑行','公交','开车'],
+//               textStyle: {
+//                 color: '#ffffff'
+//               }
+  
+//             },
+//             series: [{
+//             label: {
+//                 show: false,
+//                 position: 'center'
+//               },
+//             labelLine: {
+//                 show: false
+//               },
+//             avoidLabelOverlap: false,
+//             emphasis: {
+//                 label: {
+//                   show: true,
+//                   fontSize: '20',
+//                   fontWeight: 'bold',
+//                   fontColor: '#ffffff',
+//                   formatter: '{b} : {c}'
+//                 }
+//               },
+//               type: 'pie',
+//               center: ['50%', '50%'],
+//               radius: ['60%', '90%'],
+        
+//               data: [{
+//                 value: cycling,
+//                 name: '步行/骑行'
+//               }, {
+//                 value: pub_transit,
+//                 name: '公交'
+//               }, {
+//                 value: driving,
+//                 name: '开车'
+//               }]
+//             }]
+//           };
+//           chart.setOption(option);
+//         }
+//       })
+//     }
+//   })
+//   return chart;
+// }
 function initChart(canvas, width, height, dpr) {
   const chart = echarts.init(canvas, null, {
     width: width,
     height: height,
-    devicePixelRatio: dpr // new
+    devicePixelRatio: dpr
   });
   canvas.setChart(chart);
-  const db = wx.cloud.database()
-  const _ = db.command
-  var cycling = 0
-  var pub_transit = 0
-  var driving = 0
+  const db = wx.cloud.database();
+  const _ = db.command;
+  const $ = db.command.aggregate;
+  var { firstDayOfWeek } = getWeekRange();
+
   wx.cloud.callFunction({
     name: 'login',
-    success: id_res => {
-      db.collection('track').where({
-        _openid: id_res.result.data._openid,
-      })
-      .orderBy('date', 'desc')
-      .get({
-        success: function (res) {
-          let list = res.data
-          console.log(id_res.result.data._openid)
-          console.log('get list:', list);
-          list.forEach(item => {
-            if (item['transport'] == "步行/自行车" || item['transport'] == "电动自行车" || item['transport'] == "步行或骑行") cycling += 1
-            else if (item['transport'] == "公交/出租车/网约车/轨道交通" || item['transport'] == "公共交通") pub_transit += 1
-            else driving += 1
+    success: async (id_res) => {
+      const userOpenId = id_res.result.data._openid;
+      const firstDayOfWeekDate = new Date(firstDayOfWeek).toJSON();
+
+      try {
+        var f = new Date(firstDayOfWeek),
+        a = $.dateFromString({
+          dateString: f.toJSON()
+        });
+        const result = await db.collection('track')
+          .aggregate()
+          .addFields({
+            matched: $.gte(["$endTime", a])
           })
-          console.log(cycling)
-          console.log(pub_transit)
-          console.log(driving)
-          var option = {
-            backgroundColor: 'rgba(0, 0, 0, 0)',
-            legend: {
-              orient: 'vertical',
-              x: 'left',
-              data: ['步行/骑行','公交','开车'],
-              textStyle: {
-                color: '#ffffff'
-              }
-  
-            },
-            series: [{
+          .match({
+            _openid: userOpenId,
+            matched:true
+          })
+          .unwind('$transport')
+          .group({
+            _id: '$transport',
+            count: $.sum(1)
+          })
+          .end();
+        console.log('result of query',result)        
+        let walking = 0;
+        let cycling = 0;
+        let pub_transit = 0;
+        let driving = 0;
+        result.list.forEach(item => {
+          if (item._id === "步行") {
+            walking = item.count;
+          } else if (item._id === "地铁" || item._id === "高铁" || item._id === "公交车") {
+            pub_transit += item.count;
+          } else if (item._id === "自行车(共享单车)" || item._id === "电动自行车") {
+            cycling += item.count;
+          } else {
+            driving += item.count;
+          }
+        });
+
+        var option = {
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          legend: {
+            orient: 'vertical',
+            x: 'left',
+            data: ['步行', '骑行', '公交', '开车'],
+            textStyle: {
+              color: '#ffffff'
+            }
+          },
+          series: [{
             label: {
-                show: false,
-                position: 'center'
-              },
+              show: false,
+              position: 'center'
+            },
             labelLine: {
-                show: false
-              },
+              show: false
+            },
             avoidLabelOverlap: false,
             emphasis: {
-                label: {
-                  show: true,
-                  fontSize: '20',
-                  fontWeight: 'bold',
-                  fontColor: '#ffffff',
-                  formatter: '{b} : {c}'
-                }
-              },
-              type: 'pie',
-              center: ['50%', '50%'],
-              radius: ['60%', '90%'],
-        
-              data: [{
-                value: cycling,
-                name: '步行/骑行'
-              }, {
-                value: pub_transit,
-                name: '公交'
-              }, {
-                value: driving,
-                name: '开车'
-              }]
+              label: {
+                show: true,
+                fontSize: '20',
+                fontWeight: 'bold',
+                fontColor: '#ffffff',
+                formatter: '{b} : {c}'
+              }
+            },
+            type: 'pie',
+            center: ['50%', '50%'],
+            radius: ['60%', '90%'],
+            data: [{
+              value: walking,
+              name: '步行'
+            }, 
+            {
+              value: cycling,
+              name: '骑行'
+            },
+            {
+              value: pub_transit,
+              name: '公交'
+            }, {
+              value: driving,
+              name: '开车'
             }]
-          };
-          chart.setOption(option);
-        }
-      })
+          }]
+        };
+
+        chart.setOption(option);
+      } catch (error) {
+        console.error('Error getting transportation data:', error);
+      }
     }
-  })
+  });
   return chart;
 }
+
 Page({
 
   /**
